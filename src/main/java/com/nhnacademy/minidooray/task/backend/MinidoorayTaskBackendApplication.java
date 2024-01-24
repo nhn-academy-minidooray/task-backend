f0ddeae095d8778340e916ebd5252e35787a12b0
dduneon <dduneon@gmail.com>
2024-01-24 23:38:11 +0900

    [INIT] INIT PROJECT

---
+++ src/main/java/com/nhnacademy/minidooray/task/backend/MinidoorayTaskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray.task.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinidoorayTaskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinidoorayTaskBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/minidooray/task/backend/MinidoorayTaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray.task.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinidoorayTaskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
