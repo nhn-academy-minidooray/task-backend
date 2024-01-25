bf29c81bb0f057f1265007dc3a68eef336915d6c
hyeonjaez <jhjh0022@naver.com>
2024-01-25 22:23:24 +0900

    refactor: ProjectController

--- src/main/java/com/nhnacademy/minidooray/task/backend/controller/ProjectController.java
+++ src/main/java/com/nhnacademy/minidooray/task/backend/controller/ProjectController.java
@@ -1,17 +1,16 @@
 package com.nhnacademy.minidooray.task.backend.controller;
 
 import com.nhnacademy.minidooray.task.backend.domain.ProjectDto;
-import com.nhnacademy.minidooray.task.backend.domain.ProjectRegistryRequest;
-import com.nhnacademy.minidooray.task.backend.entity.Project;
+import com.nhnacademy.minidooray.task.backend.domain.ProjectRequest;
 import com.nhnacademy.minidooray.task.backend.service.ProjectService;
 import java.util.List;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,24 +24,21 @@ public class ProjectController {
     }
 
     @PostMapping("/list")
-    public ResponseEntity<List<ProjectDto>> projectList() {
-        List<ProjectDto> projectListByAccountId = projectService.getProjectListByAccountId("");
-
-        return ResponseEntity.ok().body(projectListByAccountId);
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProjectDto> projectList() {
+        return projectService.getProjectListByAccountId("");
     }
 
     @PostMapping("/register")
-    public ResponseEntity<Project> createProject(@RequestBody ProjectRegistryRequest projectRegistryRequest) {
-        Project project = projectService.createProject(projectRegistryRequest, "");
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(project);
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProject(@RequestBody ProjectRequest projectRequest) {
+        projectService.createProject(projectRequest);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProjectDto> getProject(@PathVariable("id") Long projectId) {
-        ProjectDto projectDto = projectService.getProjectDtoById(projectId);
-
-        return ResponseEntity.ok().body(projectDto);
+    @ResponseStatus(HttpStatus.OK)
+    public ProjectDto getProject(@PathVariable("id") Long projectId) {
+        return projectService.getProjectDtoById(projectId);
     }
 
 }
