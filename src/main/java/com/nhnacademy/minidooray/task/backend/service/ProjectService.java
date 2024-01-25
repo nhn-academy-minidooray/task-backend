7d7f236f5d48b9f0cce3b39d5221cf1eceb03869
hyeonjaez <jhjh0022@naver.com>
2024-01-25 22:25:39 +0900

    feat: milestone service layer

--- src/main/java/com/nhnacademy/minidooray/task/backend/service/ProjectService.java
+++ src/main/java/com/nhnacademy/minidooray/task/backend/service/ProjectService.java
@@ -1,16 +1,24 @@
 package com.nhnacademy.minidooray.task.backend.service;
 
+import com.nhnacademy.minidooray.task.backend.domain.MilestoneDto;
+import com.nhnacademy.minidooray.task.backend.domain.MilestoneRequest;
 import com.nhnacademy.minidooray.task.backend.domain.ProjectDto;
-import com.nhnacademy.minidooray.task.backend.domain.ProjectRegistryRequest;
-import com.nhnacademy.minidooray.task.backend.entity.Project;
+import com.nhnacademy.minidooray.task.backend.domain.ProjectRequest;
 import java.util.List;
 
 public interface ProjectService {
-    Project createProject(ProjectRegistryRequest projectRegistryRequest, String adminId);
+    void createProject(ProjectRequest projectRequest);
 
     List<ProjectDto> getProjectListByAccountId(String accountId);
 
     ProjectDto getProjectDtoById(Long projectId);
 
+    void createMileStone(MilestoneRequest milestoneRequest);
+
+    List<MilestoneDto> getMilestoneList();
+
+    MilestoneDto getMilestoneById(Long milestoneId);
+
+    void deleteMilestone(Long milestoneId);
 
 }
