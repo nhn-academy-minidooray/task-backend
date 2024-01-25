d85e2e878c3ced010949a34f1922042afa333259
hyeonjaez <jhjh0022@naver.com>
2024-01-25 22:24:32 +0900

    refactor: method name

--- src/main/java/com/nhnacademy/minidooray/task/backend/domain/ProjectDto.java
+++ src/main/java/com/nhnacademy/minidooray/task/backend/domain/ProjectDto.java
@@ -1,7 +1,10 @@
 package com.nhnacademy.minidooray.task.backend.domain;
 
 public interface ProjectDto {
-    Long getProjectId();
 
-    String getProjectName();
+    Long getId();
+
+    String getName();
+
+
 }
