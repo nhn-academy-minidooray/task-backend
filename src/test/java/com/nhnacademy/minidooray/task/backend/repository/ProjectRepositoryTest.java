0aeaeded05d6636e32567e8f349414e1f5c27a2a
hyeonjaez <jhjh0022@naver.com>
2024-01-27 17:52:20 +0900

    test : ProjectRepository

--- src/test/java/com/nhnacademy/minidooray/task/backend/repository/ProjectRepositoryTest.java
+++ src/test/java/com/nhnacademy/minidooray/task/backend/repository/ProjectRepositoryTest.java
@@ -1,53 +1,62 @@
 package com.nhnacademy.minidooray.task.backend.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-
-import com.nhnacademy.minidooray.task.backend.domain.ProjectRequest;
+import com.nhnacademy.minidooray.task.backend.domain.ProjectDto;
 import com.nhnacademy.minidooray.task.backend.entity.Project;
-
-import com.nhnacademy.minidooray.task.backend.service.ProjectService;
-import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-@SpringBootTest
+
+@DataJpaTest
 class ProjectRepositoryTest {
-    @Autowired
-    ProjectService projectService;
 
     @Autowired
-    ProjectRepository projectRepository;
+    TestEntityManager testEntityManager;
 
-
-    @Test
-    void test(){
-        ProjectRequest projectRegistryRequest = new ProjectRequest("name", "id");
-        projectService.createProject(projectRegistryRequest);
-    }
-
-//    @Test
-//    void test1(){
-//
-//        List<ProjectDto> jkjk = projectService.getProjectListByAccountId("jkjk");
-//
-//        System.out.println("00-0--00----0-0-0-0--0-0-");
-//        System.out.println("00-0--00----0-0-0-0--0-0-");
-//        System.out.println("00-0--00----0-0-0-0--0-0-");
-//        System.out.println("00-0--00----0-0-0-0--0-0-");
-//        System.out.println("00-0--00----0-0-0-0--0-0-");
-//        System.out.println("00-0--00----0-0-0-0--0-0-");
-//        System.out.println("00-0--00----0-0-0-0--0-0-");
-//        jkjk.stream().forEach(System.out::println);
-//
-//
-//    }
+    @Autowired
+    private ProjectRepository projectRepository;
 
     @Test
-    void as(){
-        projectRepository.getProjectListById("jkjk").stream().forEach(System.out::println);
+    void d() {
+        Project project = testProjectBuilder();
+
+
     }
+
+    @Test
+    void findProjectByIdTest() {
+        Project saveProject = testEntityManager.persist(testProjectBuilder());
+
+        ProjectDto findProject = projectRepository.findProjectById(saveProject.getId()).orElse(null);
+
+        Assertions.assertNotNull(findProject);
+        Assertions.assertEquals(findProject.getId(), saveProject.getId());
+        Assertions.assertEquals(findProject.getName(), saveProject.getName());
+    }
+
+    @Test
+    void getProjectByIdTest(){
+        Project saveProject = testEntityManager.persist(testProjectBuilder());
+
+        Project findProject = projectRepository.getProjectById(saveProject.getId());
+
+        Assertions.assertNotNull(findProject);
+        Assertions.assertEquals(findProject.getId(), saveProject.getId());
+        Assertions.assertEquals(findProject.getName(), saveProject.getName());
+        Assertions.assertEquals(findProject.getStatus(), saveProject.getStatus());
+        Assertions.assertEquals(findProject.getAdminId(), saveProject.getAdminId());
+    }
+
+    private static Project testProjectBuilder() {
+        return Project.builder()
+                .name("dooray")
+                .status("활성")
+                .adminId("jkjk")
+                .build();
+    }
+
 
 }
